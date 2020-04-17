e663a1e1040abf589c4234e72cca897017cf50c7
lukeputz92 <putzey@gmail.com>
2020-04-16 21:04:10 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+
+const tasksReducer = (state =[], action) => {
+  switch(action.type) {
+    case 'ADD_TASK':
+      let index = state.findIndex(task => task.title === action.task.title);
+      if(index === -1)
+          return [...state, action.task];
+      return state.map(
+        task => task.title === action.task.title ? {title: task.title, answers: {...task.answers, ...action.task.answers} } : task);
+      default:
+        return state;
+
+  }
+}
+
+const store = createStore(tasksReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
